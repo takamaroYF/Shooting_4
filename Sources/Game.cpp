d815c89bb2dd77d560ed3d91d4669278c2012143
hw16a114 <hw16a114@oecu.jp>
2017-11-28 16:13:29 +0900

    BGMを追加しました

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -26,6 +26,9 @@ void Start()
     targetRect = Rect(80, -140, 40, 40);
     bulletPos.x = -999;
     score = 0;
+    
+    PlayBGM("bgm_maoudamashii_8bit07.mp3"); //BGMを追加（HW16A114 田中陽大）
+
 }
 
 // 1/60秒ごとに呼ばれる関数です。モデルの更新と画面の描画を行います。
